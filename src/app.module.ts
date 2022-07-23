e13aa47ffa6bc6da21f3b5953c9831eb5efc744a
Max Andrade <maxreisandrade@gmail.com>
2022-07-23 20:51:37 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PetsModule } from './pets/pets.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pet } from './pets/pet.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [Pet],
+        synchronize: true,
+
+      }),
+      inject: [ConfigService],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+    PetsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ src/pets/pets.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePetInput } from './dto/create-pet.input';
+import { Pet } from './pet.entity';
+
+@Injectable()
+export class PetsService {
+  constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>) { }
+
+  async createPet(createPetInput: CreatePetInput): Promise<Pet> {
+    const newPet = this.petsRepository.create(createPetInput)
+    return this.petsRepository.save(newPet)
+  }
+
+  async findAll(): Promise<Pet[]> {
+    return this.petsRepository.find({})
+  }
+
+  async findOne(id: number): Promise<Pet> {
+    return this.petsRepository.findOneOrFail({ where: { id } })
+  }
+}
