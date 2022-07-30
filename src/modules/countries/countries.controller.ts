cce3cef80180e5d37b23f8e88aacdfe6a1afb3ec
Max Andrade <maxreisandrade@gmail.com>
2022-07-30 12:41:12 -0300

    fix

--- src/modules/countries/countries.controller.ts
+++ src/modules/countries/countries.controller.ts
@@ -10,9 +10,9 @@ import * as translate from '@vitalets/google-translate-api';
 export class CountriesController {
   constructor(readonly countriesService: CountriesService) {}
 
-  @Post('/scrap-enUS')
+  @Post('/scrap')
   async scrapCountries(): Promise<void> {
-    const countries: Country[] = [];
+    // const countries: Country[] = [];
     const response = await firstValueFrom(
       new HttpService().get<string>(
         'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population'
@@ -21,38 +21,37 @@ export class CountriesController {
 
     const $ = cheerio.load(response.data);
     const rows = $('.wikitable tbody tr');
-    rows.each((i, row) => {
-      const cells = $(row).children();
-      const firstCellInRow = $(cells.get(0)).text();
-      const isACountry = !isNaN(parseInt(firstCellInRow));
-      if (isACountry) {
-        const countryName = $(cells.get(1)).text().trim();
-        const flag = $(cells.get(1))
-          .find('img')
-          .attr('srcset')
-          .split(' ')[2]
-          .substring(2);
-        // countries.push({
-        //   enUS: countryName,
-        //   flag: `https://${flag}`
-        // });
-      }
-    });
-    await this.countriesService.addCountries(countries);
-  }
+    const countries = await Promise.all(
+      rows
+        .toArray()
+        .slice(0, 5)
+        .map(async (row) => {
+          const cells = $(row).children();
+          const firstCellInRow = $(cells.get(0)).text();
+          const isACountry = !isNaN(parseInt(firstCellInRow));
+          // if (!isACountry) return;
 
-  @Post('translate-ptBR')
-  async translateCountriesptBR(): Promise<void> {
-    const countries = (await this.countriesService.findAll()).slice(0, 5);
-    const translated = await Promise.all(
-      countries.map(async ({ id, enUS }) => {
-        const ptBR = await translate(enUS, { to: 'pt' });
-        return {
-          id,
-          ptBR: ptBR.text
-        };
-      })
+          // const enUS = $(cells.get(1)).text().trim();
+          // const ptBR = await translate(enUS, { to: 'pt' }).then(
+          //   (res) => res.text
+          // );
+          // const flag = $(cells.get(1))
+          //   .find('img')
+          //   .attr('srcset')
+          //   .split(' ')[2]
+          //   .substring(2);
+
+          // return { enUS, ptBR, flag: `https://${flag}` };
+
+          // countries.push({
+          //   enUS,
+          //   ptBR,
+          //   flag: `https://${flag}`,
+          // });
+        })
     );
+    // console.log(countries);
+    // await this.countriesService.addCountries(countries);
   }
 
   @Delete('')
