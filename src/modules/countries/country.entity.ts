01eea32fe9a1db79444b873a7cdef2684588cedc
Max Andrade <maxreisandrade@gmail.com>
2022-07-30 12:38:28 -0300

    fix contry entity

--- src/modules/countries/country.entity.ts
+++ src/modules/countries/country.entity.ts
@@ -1,12 +1,9 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity()
 @ObjectType()
 export class Country {
-  @PrimaryGeneratedColumn()
-  id: string;
-
   @Column()
   @Field()
   enUS: string;
@@ -14,8 +11,4 @@ export class Country {
   @Column()
   @Field()
   flag: string;
-
-  @Column({ nullable: true })
-  @Field({})
-  ptBR: string;
 }
